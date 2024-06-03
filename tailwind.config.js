1fca3116e87c32d89446ee52b127cb5a99de824e
rahat404 <iquramulislam.rahat@gmail.com>
2024-06-03 22:12:30 +0600

    Banner and extra banner section done also responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage:
+        {
+          'bg-image1': "url('/images/banner1.png')",
+          'bg-image2': "url('/images/banner2.png')",
+          'bg-image3': "url('/images/banner3.png')",
+          'bg-image4': "url('/images/banner4.png')",
+        }
+      },
     },
     plugins: [],
   }
